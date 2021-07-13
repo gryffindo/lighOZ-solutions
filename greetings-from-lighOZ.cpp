acbe2b997253aafe6d81d9d6fd3775cc6b796b92
Sakib Al Hasan <76560286+saifsaki@users.noreply.github.com>
2021-07-14 01:33:55 +0600

    solved greetings from lighOZ

---
+++ greetings-from-lighOZ.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int cases, caseno;
+  cin >> cases;
+  for(caseno = 1; caseno <= cases; ++caseno ) {
+    int a, b;
+    cin >> a >> b;
+    cout << "Case " << caseno << ": " << a+b << endl;
+  }
+  return 0;
+}
+
+// Problem Link: https://lightoj.com/problem/greetings-from-lightoj
